04bedb2192fdebddddcd249e598622870a00c10c
nox <noxabellus@gmail.com>
2025-12-29 19:57:44 -0600

    update to 0.15.2 and new rg, add flake.nix, change versioning system to zig convention

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Zig 0.15.2 overlay flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
+    zls-overlay.url = "github:zigtools/zls/0.15.1";
+    zls-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { nixpkgs, zig-overlay, zls-overlay, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      zig = zig-overlay.packages.${system}."0.15.2";
+      zls = zls-overlay.packages.${system}.default;
+    in {
+      formatter.${system} = pkgs.alejandra;
+
+      packages.${system} = {
+        default = zig;
+        zls = zls;
+        zig = zig;
+      };
+
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [ zig zls pkgs.bashInteractive ];
+
+        shellHook = ''
+          echo "Zig version: ${zig.version}"
+          echo "ZLS version: ${zls.version}"
+
+          export PROMPT_NAME='dev:zig@${zig.version}';
+        '';
+      };
+    };
+}
